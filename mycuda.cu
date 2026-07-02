8f32460d65fa000513cadbd4f42867a0efb2420a
zliu <zgliu71@gmail.com>
2026-07-02 01:18:19 -0700

    Fix indexing bug in cuda_vector_add kernel to prevent GPU launch timeout

--- mycuda.cu
+++ mycuda.cu
@@ -92,7 +92,7 @@ __global__ void cuda_vector_add(float *out, float *a, float *b, int n) {
     int stride = blockDim.x * gridDim.x;
 
     for (int i = index; i < n; i += stride) {
-        out[index] = a[index] + b[index];
+        out[i] = a[i] + b[i];
     }
 }
 
